59f18d61243c9e930a67efc2f2a40cfe64924771
Sezer Karaoglanoglu <info@darkson.tech>
2022-12-18 10:44:02 +0100

    Added retain=True, websockets support

--- app/MQTTClient.py
+++ app/MQTTClient.py
@@ -14,7 +14,7 @@ class MQTTClient():
             else:
                 logging.error(f"Could not connect to MQTT-Broker. Error-Code {rc}")
         
-        self.client = mqtt_client.Client(f'rgb_pwm_mqtt-{random.randint(0, 1000)}')
+        self.client = mqtt_client.Client(f'rgb_pwm_mqtt-{random.randint(0, 1000)}', transport=env.get("MQTT_TRANSPORT"), reconnect_on_failure=True)
         self.client.username_pw_set(env.get("MQTT_USERNAME"), env.get("MQTT_PASSWORD"))
         self.client.on_connect = on_connect
         self.client.will_set(env.get("MQTT_TOPIC") + "/availability", payload="offline", qos=0, retain=True)
@@ -23,7 +23,7 @@ class MQTTClient():
         self.client.connect(self.env.get("MQTT_HOST"), int(self.env.get("MQTT_PORT")))
 
     def publish(self, topic, message):
-        result = self.client.publish(topic, message)
+        result = self.client.publish(topic, message, qos=0, retain=True)
         status = result[0]
         if status != 0:
             logging.error(f"Failed to send MQTT-message to topic {topic}")
